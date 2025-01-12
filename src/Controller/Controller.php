7a5f20045beba8205936b4d10283430f90a59aa4
VicDeo <victor.dubiniuk@gmail.com>
2025-01-12 20:31:23 +0100

    Read parameters from the query string

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -6,6 +6,8 @@ namespace Task1\Controller;
 use Task1\App;
 abstract class Controller
 {
+    protected ?array $urlParams;
+
     public function __construct()
     {
         include_once '../functions.php';
@@ -14,5 +16,19 @@ abstract class Controller
     protected function getApp(): App
     {
         return App::getInstance();
+    }
+
+    protected function getUrlParam(string $name, mixed $default)
+    {
+        if (!isset($this->urlParams)) {
+            $query = $this->getApp()
+                ->getRequest()
+                ->getUri()
+                ->getQuery()
+            ;
+            parse_str($query, $this->urlParams);
+        }
+
+        return $this->urlParams[$name] ?? $default;
     }
 }
