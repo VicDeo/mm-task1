5016bfb44039cafc7a624a0ba75c1764ec227b3f
VicDeo <victor.dubiniuk@gmail.com>
2025-01-15 01:40:11 +0100

    Refactor frontend into a custom element

--- src/view/page.php
+++ src/view/page.php
@@ -7,19 +7,6 @@
     <title>Task 1</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <style>
-        th {
-            cursor: pointer
-        }
-
-        th.sort-asc:before {
-            content: " \2193"
-        }
-
-        th.sort-desc:before {
-            content: " \2191"
-        }
-    </style>
 </head>
 
 <body>
@@ -32,9 +19,16 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-link disabled" data-step="0" href="#">Raport Nadpłaty / Excess Payments</a>
-                    <a class="nav-link disabled" data-step="1" href="#">Raport Niedopłaty / Underpayment</a>
-                    <a class="nav-link disabled" data-step="2" href="#">Raport Nierozliczone faktury po terminie
+                    <a class="nav-link disabled" data-url="<?= link_to_route('report_excess') ?>"
+                        title="Raport Nadpłaty / Excess Payments report" href="#">Raport
+                        Nadpłaty / Excess Payments</a>
+                    <a class="nav-link disabled" data-url="<?= link_to_route('report_underpayment') ?>"
+                        title="Raport Niedopłaty / Underpayment Report" href="#">Raport
+                        Niedopłaty / Underpayment</a>
+                    <a class="nav-link disabled" data-url="<?= link_to_route('report_outstanding') ?>"
+                        title="Raport Nierozliczone faktury po terminie płatnośći / Report Outstanding invoices after payment due date"
+                        href="#">Raport
+                        Nierozliczone faktury po terminie
                         płatnośći / Outstanding invoices after payment due date</a>
                 </div>
             </div>
@@ -52,26 +46,173 @@
     </div>
     <script>
         window.onload = function () {
-            const steps = [
-                {
-                    "title": "Raport Nadpłaty / Excess Payments report",
-                    "action": "<?= link_to_route('report_excess') ?>",
-                    "sort_by": {},
-                    "filter_by": {}
-                },
-                {
-                    "title": "Raport Niedopłaty / Underpayment Report",
-                    "action": "<?= link_to_route('report_underpayment') ?>",
-                    "sort_by": {},
-                    "filter_by": {}
-                },
-                {
-                    "title": "Raport Nierozliczone faktury po terminie płatnośći / Report Outstanding invoices after payment due date",
-                    "action": "<?= link_to_route('report_outstanding') ?>",
-                    "sort_by": {},
-                    "filter_by": {}
+            class ReportTable extends HTMLElement {
+                constructor() {
+                    super();
+
+                    if (this.hasAttribute('src')) {
+                        this.src = this.getAttribute('src');
+                    }
+
+                    if (!this.src) return;
+
+                    this.sortField = this.hasAttribute('sortField') ? this.getAttribute('sortField') : '';
+                    this.sortDirection = this.hasAttribute('sortDirection') ? this.getAttribute('sortDirection') : '';
+                    this.filters = {};
+
+                    this.onToggleDirection = this.onToggleDirection.bind(this);
+                    this.onApplyFilter = this.onApplyFilter.bind(this);
+
+                    const shadow = this.attachShadow({
+                        mode: 'open'
+                    });
+
+                    const style = document.createElement('style');
+                    style.textContent = `
+th {
+    cursor: pointer
+}
+
+th.sort-asc:before {
+    content: " \u2193"
+}
+
+th.sort-desc:before {
+    content: " \u2191"
+    }
+                    `;
+                    shadow.appendChild(style);
+
+                    const table = document.createElement('table');
+                    table.classList.add("table", "table-bordered");
+                    const thead = document.createElement('thead');
+                    const tbody = document.createElement('tbody');
+                    table.append(thead, tbody);
+                    shadow.append(table);
                 }
-            ];
+
+                attributeChangedCallback(name, oldValue, newValue) {
+                    if (name === 'src') {
+                        this.src = newValue;
+                        this.load();
+                    }
+                }
+
+                async load() {
+                    showSpinner(true);
+                    const url = new URL(this.src);
+                    url.searchParams.set("sort_by", this.sortField);
+                    url.searchParams.set("sort_dir", this.sortDirection);
+
+                    let filterCount = 0;
+                    for (let i in this.filters) {
+                        filterCount++;
+                        url.searchParams.set("filter_" + filterCount, i);
+                        url.searchParams.set("filter_value_" + filterCount, this.filters[i]);
+                    }
+                    let result = await fetch(url);
+                    this.data = await result.json();
+                    this.render();
+                    showSpinner(false);
+                }
+
+                static get observedAttributes() {
+                    return [
+                        'src'
+                    ];
+                }
+
+                onToggleDirection(evt) {
+                    if (evt.target.innerText === this.sortField) {
+                        if (this.sortDirection === 'ASC') {
+                            this.sortDirection = 'DESC'
+                        } else {
+                            this.sortDirection = 'ASC'
+                        }
+                    } else {
+                        this.sortField = evt.target.innerText
+                        this.sortDirection = 'ASC'
+                    }
+                    this.load();
+                }
+
+                onApplyFilter(evt) {
+                    if (evt.key === "Enter"
+                        && evt.target.dataset.filter) {
+                        this.filters[evt.target.dataset.filter] = evt.target.value;
+                        this.load();
+                    }
+                }
+
+                render() {
+                    console.log(this.data);
+                    this.renderHeader();
+                    this.renderBody();
+                }
+
+                renderHeader() {
+                    const table = this.shadowRoot.querySelector('table');
+                    table.querySelector('caption') && table.querySelector('caption').remove();
+
+                    if (this.hasAttribute('title')) {
+                        const caption = document.createElement('caption');
+                        caption.textContent = this.getAttribute('title');
+                        table.append(caption);
+                    }
+
+                    const newHeading = document.createElement("tr");
+                    for (const columnName in this.data.data[0]) {
+                        const newColumn = document.createElement("th");
+                        newColumn.textContent = columnName;
+                        newColumn.classList.add('sortable');
+                        if (columnName === this.sortField) {
+                            const className = this.sortDirection === 'DESC' ? 'sort-desc' : 'sort-asc';
+                            newColumn.classList.add(className);
+                        }
+                        newHeading.appendChild(newColumn);
+                    }
+                    const thead = this.shadowRoot.querySelector('thead');
+                    thead.innerHTML = '';
+                    thead.appendChild(newHeading);
+
+                    const filterRow = document.createElement("tr");
+                    for (const columnName in this.data.data[0]) {
+                        const newColumn = document.createElement("td");
+                        newColumn.classList.add('filterable');
+                        const input = document.createElement("input");
+                        input.dataset.filter = columnName;
+                        if (typeof this.filters[columnName] !== 'undefined') {
+                            input.value = this.filters[columnName];
+                        }
+
+                        newColumn.appendChild(input);
+                        filterRow.appendChild(newColumn);
+                    }
+                    thead.append(filterRow);
+
+                    this.shadowRoot.querySelectorAll('th.sortable').forEach(t => {
+                        t.addEventListener('click', this.onToggleDirection, false);
+                    });
+
+                    this.shadowRoot.querySelectorAll('td.filterable input').forEach(t => {
+                        t.addEventListener('keyup', this.onApplyFilter, false);
+                    });
+                }
+
+                renderBody() {
+                    const tbody = this.shadowRoot.querySelector('tbody');
+                    tbody.innerHTML = '';
+                    for (const row of this.data.data) {
+                        const newRow = document.createElement("tr");
+                        for (const columnName in row) {
+                            const newColumn = document.createElement("td");
+                            newColumn.textContent = row[columnName];
+                            newRow.appendChild(newColumn);
+                        }
+                        tbody.appendChild(newRow);
+                    }
+                }
+            }
 
             fetch("<?= link_to_route('db_create') ?>")
                 .then(() => {
@@ -82,162 +223,26 @@
                     document.getElementsByClassName("nav-link")[0].click();
                 });
 
-            async function getData(url, cb) {
-                try {
-                    const response = await fetch(url);
-                    if (!response.ok) {
-                        throw new Error(`Response status: ${response.status}`);
-                    }
-
-                    const json = await response.json();
-                    cb(json);
-                } catch (error) {
-                    console.error(error.message);
-                }
-            }
-
             const showSpinner = (state) => {
                 const spinner = document.getElementsByClassName("spinner-wrapper")[0];
                 spinner.style.display = state ? 'flex' : 'none';
-                if (state) {
-                    spinner.classList.add("d-flex");
-                    for (const link of document.getElementsByClassName("nav-link")) {
-                        link.classList.add("disabled");
-                    }
-                } else {
-                    spinner.classList.remove("d-flex");
-                    for (const link of document.getElementsByClassName("nav-link")) {
-                        link.classList.remove("disabled");
-                    }
+                const method = state ? 'add' : 'remove';
+                spinner.classList[method]("d-flex");
+                for (const link of document.getElementsByClassName("nav-link")) {
+                    link.classList[method]("disabled");
                 }
-            }
-
-            const applyFilter = (event) => {
-                if (event.key === "Enter"
-                    && event.target.dataset.filter
-                    && event.target.dataset.step
-                ) {
-                    const reportId = event.target.dataset.step;
-                    steps[reportId]['filter_by'][event.target.dataset.filter] = event.target.value;
-                    document.getElementsByClassName("nav-link")[reportId].click();
-                }
-            }
-
-            const applySort = (event) => {
-                const th = event.target;
-                const table = th.closest('table');
-                const reportId = table.dataset.step;
-                const field = th.textContent;
-                if (steps[reportId]['sort_by'][field] === 'ASC') {
-                    steps[reportId]['sort_by'][field] = 'DESC';
-                } else {
-                    steps[reportId]['sort_by'] = {};
-                    steps[reportId]['sort_by'][field] = 'ASC';
-                }
-
-                const className = th.classList.contains('sort-asc') ? 'sort-desc' : 'sort-asc';
-                for (const el of th.parentNode.children) {
-                    el.classList.remove('sort-asc', 'sort-desc');
-                };
-                th.classList.add(className);
-                document.getElementsByClassName("nav-link")[reportId].click();
-            }
-
-            const loadReport = (event) => {
-                event.target.setAttribute('disabled', 'disabled');
-                const currentStep = parseInt(event.target.dataset.step);
-                const onResponse = function (json) {
-                    console.log(json);
-                    const metadata = steps[currentStep];
-                    metadata['id'] = currentStep;
-                    showReport(metadata, json.data);
-                    showSpinner(false);
-                }
-
-                showSpinner(true);
-                console.log(steps[currentStep]);
-                const url = new URL(steps[currentStep]['action']);
-                for (let i in steps[currentStep]["sort_by"]) {
-                    url.searchParams.set("sort_by", i);
-                    url.searchParams.set("sort_dir", steps[currentStep]["sort_by"][i]);
-                }
-                let filterCount = 0;
-                for (let i in steps[currentStep]["filter_by"]) {
-                    filterCount++;
-                    url.searchParams.set("filter_" + filterCount, i);
-                    url.searchParams.set("filter_value_" + filterCount, steps[currentStep]["filter_by"][i]);
-                }
-
-                getData(url, onResponse);
-            }
-
-            const showReport = (metadata, data) => {
-                const newTitle = document.createElement("h4");
-                newTitle.textContent = metadata['title'];
-
-                const newTable = document.createElement("table");
-                newTable.classList.add("table", "table-bordered");
-                newTable.dataset.step = metadata["id"];
-
-                const thead = document.createElement("thead");
-                const newHeading = document.createElement("tr");
-                for (columnName in data[0]) {
-                    const newColumn = document.createElement("th");
-                    newColumn.textContent = columnName;
-                    newColumn.classList.add('sortable');
-                    if (typeof metadata["sort_by"][columnName] !== 'undefined') {
-                        const className = metadata["sort_by"][columnName] === 'DESC' ? 'sort-desc' : 'sort-asc';
-                        newColumn.classList.add(className);
-                    }
-                    newHeading.appendChild(newColumn);
-                }
-                thead.appendChild(newHeading);
-                newTable.appendChild(thead);
-
-                const tbody = document.createElement("tbody");
-                const filterRow = document.createElement("tr");
-                for (columnName in data[0]) {
-                    const newColumn = document.createElement("td");
-                    const input = document.createElement("input");
-                    input.dataset.filter = columnName;
-                    input.dataset.step = metadata['id'];
-                    if (typeof metadata['filter_by'][columnName] !== 'undefined') {
-                        input.value = metadata['filter_by'][columnName];
-                    }
-                    newColumn.appendChild(input);
-                    filterRow.appendChild(newColumn);
-                }
-                tbody.append(filterRow);
-                newTable.append(tbody);
-
-                for (row of data) {
-                    const newRow = document.createElement("tr");
-                    for (columnName in row) {
-                        const newColumn = document.createElement("td");
-                        newColumn.textContent = row[columnName];
-                        newRow.appendChild(newColumn);
-                    }
-                    tbody.appendChild(newRow);
-                }
-
-                const target = document.getElementById('report-data');
-                target.innerHTML = '';
-                target.appendChild(newTitle);
-                target.appendChild(newTable);
             }
 
             const body = document.getElementsByTagName("body")[0];
-            body.addEventListener("keyup", applyFilter);
             body.addEventListener("click", (event) => {
                 if (event.target.classList.contains("nav-link")
-                    && typeof event.target.dataset.step === 'string'
+                    && typeof event.target.dataset.url === 'string'
                 ) {
-                    loadReport(event);
-                }
-                if (event.target.classList.contains("sortable")) {
-                    applySort(event);
+                    const target = document.getElementById('report-data');
+                    target.innerHTML = `<report-table src="${event.target.dataset.url}" title="${event.target.getAttribute('title')}">`
                 }
             });
+            customElements.define('report-table', ReportTable);
         }
     </script>
 </body>
